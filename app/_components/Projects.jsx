4b77508a770cea8827252d97c3e2251a523c6397
ParzivalFR <parzivaleu@gmail.com>
2024-05-12 18:15:22 +0200

    Add Request Backend

--- app/_components/Projects.jsx
+++ app/_components/Projects.jsx
@@ -1,63 +1,121 @@
 "use client";
-import Link from "next/link";
+
+import ky from "ky";
 import { useEffect, useState } from "react";
-import { RiExternalLinkLine } from "react-icons/ri";
+import { FaHeart } from "react-icons/fa";
+import { v4 as uuidv4 } from "uuid";
+import { useFetch } from "../hooks/useFetch";
 
 const Projects = () => {
-  const [projects, setProjects] = useState([]);
+  const { fetchedData, isLoading, error } = useFetch(
+    "http://185.157.247.55:3005/api/projects"
+  );
+
+  const [heartStates, setHeartStates] = useState({});
+  const [userIp, setUserIp] = useState("");
+  const [likes, setLikes] = useState({});
 
   useEffect(() => {
-    fetch("http://localhost:3005/api/projects")
-      .then((response) => response.json())
-      .then((data) => setProjects(data));
-  }, []);
+    if (typeof window !== "undefined") {
+      const localUserIp = localStorage.getItem("userIp");
+      if (!localUserIp) {
+        const newUserIp = uuidv4(); // Génère un identifiant unique lors du montage du composant
+        setUserIp(newUserIp);
+        localStorage.setItem("userIp", newUserIp);
+      } else {
+        setUserIp(localUserIp);
+      }
+    }
+
+    // Récupérer les likes pour chaque projet
+    if (fetchedData && Array.isArray(fetchedData)) {
+      fetchedData.forEach(async (project) => {
+        const response = await ky.get(
+          `http://185.157.247.55:3005/api/likes/${project._id}`
+        );
+        const data = await response.json();
+        setLikes((prevLikes) => ({
+          ...prevLikes,
+          [project._id]: data.length,
+        }));
+      });
+    }
+  }, [fetchedData]);
+
+  const toggleColor = async (id) => {
+    setHeartStates({
+      ...heartStates,
+      [id]: !heartStates[id],
+    });
+
+    try {
+      if (heartStates[id]) {
+        await ky.delete(`http://185.157.247.55:3005/api/likes/${id}`, {
+          json: { userIp, postId: id },
+        });
+        console.log("Deleted");
+      } else {
+        await ky.post(`http://185.157.247.55:3005/api/likes/${id}`, {
+          json: { userIp, postId: id },
+        });
+        console.log("Created");
+      }
+
+      // Rafraîchir les likes
+      const response = await ky.get(
+        `http://185.157.247.55:3005/api/likes/${id}`
+      );
+      const data = await response.json();
+      setLikes((prevLikes) => ({
+        ...prevLikes,
+        [id]: data.length,
+      }));
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <section
-      id="projects"
-      className="grid gap-y-20 gap-x-10 grid-cols-1 center md:grid-cols-2 lg:grid-cols-3 w-full md:w-4/5 sm:m-auto"
-    >
-      {projects &&
-        projects.map((project) => (
-          <div
-            key={project.id}
-            className="relative flex flex-col space-y-3 h-80 w-4/5 md:w-full m-auto bg-foreground/5 p-5 rounded-md"
+    <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 3xl:grid-cols-4 gap-10 p-4 w-4/5 m-auto">
+      {isLoading ? (
+        <p>Chargement...</p>
+      ) : error ? (
+        <p>Une erreur est survenue, veuillez m'excuser.</p>
+      ) : (
+        fetchedData.map((project) => (
+          <article
+            key={project._id}
+            className="relative flex flex-col justify-between gap-10 p-4 bg-foreground/5 rounded-lg transition-transform transform hover:scale-105 duration-700 shadow-pxl"
           >
-            <div className="flex items-center overflow-hidden h-3/5 w-full rounded-xl">
-              <img
-                src={project.cover.replace("/upload/", "/upload/f_webp/")}
-                alt={project.title}
-                className="w-full object-cover"
-              />
+            <div className="flex justify-center items-center object-cover">
+              <img src={project.cover} alt={project.title} />
             </div>
-            <div className="space-y-2">
-              <h3 className="text-xl font-semibold">
-                {project.title} - Projet {project.number}
-              </h3>
-              <p className="text-sm">{project.shortDescription}</p>
-              <p className="text-sm">Fait en {project.year}</p>
+            <div>
+              <h2 className="font-bold text-xl">{project.title}</h2>
+              <p>{project.shortDescription}</p>
               {project.skills && (
-                <div className="flex flex-wrap space-x-2">
+                <ul className="flex flex-wrap gap-2 w-[90%] mt-5">
                   {project.skills.map((skill) => (
-                    <span
+                    <li
                       key={skill}
-                      className="text-xs px-2 py-1 bg-foreground/10 rounded-md"
+                      className="bg-foreground/50 rounded px-1 text-background text-sm"
                     >
                       {skill}
-                    </span>
+                    </li>
                   ))}
-                </div>
+                </ul>
               )}
-              <Link
-                href={project.url}
-                target="_blank"
-                className="absolute top-2 right-2"
-              >
-                <RiExternalLinkLine size={25} />
-              </Link>
             </div>
-          </div>
-        ))}
+            <FaHeart
+              className={`absolute bottom-2 right-3 text-2xl cursor-pointer transition-all transform hover:scale-110 duration-500 ${
+                heartStates[project._id] ? "text-primary/20" : "text-primary/80"
+              }`}
+              onClick={() => toggleColor(project._id)}
+            />
+            <p>{likes[project._id] || 0} likes</p>
+          </article>
+        ))
+      )}
     </section>
   );
 };
