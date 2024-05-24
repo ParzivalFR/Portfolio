9d16079d1247e5e7d5ecea3f9f617d4c3da43435
ParzivalFR <parzivaleu@gmail.com>
2024-05-24 21:20:56 +0200

    Add icon social on footer

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -1,22 +1,43 @@
 import Link from "next/link";
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
 import VercelIcon from "../icons/VercelIcon";
+import Spacing from "./Spacing";
 
 const Footer = () => {
   const currentDate = new Date().getFullYear();
 
   return (
-    <footer className="bg-foreground/5 mt-auto">
-      <div className="flex justify-center p-5">
+    <footer className="bg-foreground/5 mt-auto py-4">
+      <div className="flex justify-center">
         <VercelIcon size={150} />
       </div>
+      <div className="flex justify-center items-center gap-2">
+        <Link
+          href={"https://github.com/parzivalfr"}
+          target="_blank"
+          className="transition duration-500 ease-in-out hover:scale-105 hover:text-primary/60"
+        >
+          <FaGithubSquare size={30} />
+        </Link>
+        <Link
+          href={"https://www.linkedin.com/in/ga%C3%ABl-richard-680b8a263/"}
+          target="_blank"
+          className="transition duration-500 ease-in-out hover:scale-105 hover:text-primary/60"
+        >
+          <FaLinkedin size={30} />
+        </Link>
+      </div>
+      <Spacing size={10} />
       <div>
-        <p className="text-center text-sm text-foreground/70 pb-5">
+        <p className="text-center text-sm text-foreground/70">
           © {currentDate} Created with ❤️ by{" "}
           <Link
             href={"https://discord.com/users/1017721923259613234"}
             target="_blank"
           >
-            <span className="text-accent-foreground">Parzival</span>
+            <span className="text-accent-foreground transition duration-500 ease-in-out hover:text-primary/60 font-semibold">
+              Parzival
+            </span>
           </Link>
           .
         </p>
