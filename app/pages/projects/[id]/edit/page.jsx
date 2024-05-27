4bed7032dc96842ce0515e9f8913febc01f2c5d8
ParzivalFR <parzivaleu@gmail.com>
2024-05-27 12:07:23 +0200

    Fix modify form

--- app/pages/projects/[id]/edit/page.jsx
+++ app/pages/projects/[id]/edit/page.jsx
@@ -8,10 +8,8 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Divider } from "@nextui-org/divider";
 import ky from "ky";
-import { marked } from "marked";
 import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { MdDeleteForever } from "react-icons/md";
 import ReactMarkdown from "react-markdown";
 import Swal from "sweetalert2";
 
@@ -29,20 +27,11 @@ const EditProjectForm = () => {
     year: "",
     skills: "",
     url: "",
+    deletedImages: [],
   });
 
-  const [currentImages, setCurrentImages] = useState([]);
-  const [currentCover, setCurrentCover] = useState(null);
-
   useEffect(() => {
-    const localUserId = localStorage.getItem("userId");
-    setFormData((prevState) => ({
-      ...prevState,
-      userId: localUserId,
-    }));
-
-    // Fetch project data
-    const fetchProject = async () => {
+    const fetchProjectData = async () => {
       try {
         const token = localStorage.getItem("token");
         if (!token) throw new Error("No token available.");
@@ -58,32 +47,36 @@ const EditProjectForm = () => {
         setFormData((prevState) => ({
           ...prevState,
           ...project,
-          skills: project.skills.join(","),
         }));
-        setCurrentImages(project.images || []);
-        setCurrentCover(project.cover || null);
       } catch (error) {
-        console.error("Error fetching project data:", error);
+        console.error("Erreur lors de la récupération du projet:", error);
         Swal.fire({
           icon: "error",
           title: "Erreur",
-          text: `Une erreur est survenue lors de la récupération des données du projet: ${error.message}`,
+          text: `Une erreur est survenue lors de la récupération du projet: ${error.message}`,
           showConfirmButton: false,
           timer: 2000,
         });
       }
     };
 
-    fetchProject();
+    fetchProjectData();
   }, [id]);
 
   const handleChange = (e) => {
     const { name, value, type, files } = e.target;
     if (type === "file") {
-      setFormData((prevState) => ({
-        ...prevState,
-        [name]: name === "images" ? [...files] : files[0],
-      }));
+      if (name === "images") {
+        setFormData((prevState) => ({
+          ...prevState,
+          images: [...prevState.images, ...files],
+        }));
+      } else {
+        setFormData((prevState) => ({
+          ...prevState,
+          [name]: files[0],
+        }));
+      }
     } else if (name === "skills") {
       setFormData((prevState) => ({
         ...prevState,
@@ -97,53 +90,37 @@ const EditProjectForm = () => {
     }
   };
 
-  const handleDeleteImage = (image) => {
-    setCurrentImages(currentImages.filter((img) => img !== image));
+  const handleRemoveImage = (image) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      images: prevState.images.filter((img) => img !== image),
+      deletedImages: [...prevState.deletedImages, image],
+    }));
   };
 
-  const handleDeleteCover = () => {
-    setCurrentCover(null);
+  const handleRemoveCover = () => {
+    setFormData((prevState) => ({
+      ...prevState,
+      cover: null,
+    }));
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    const confirmation = await Swal.fire({
-      title: "Êtes-vous sûr ?",
-      text: "Voulez-vous vraiment modifier ce projet ?",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Oui",
-      cancelButtonText: "Non",
-    });
-
-    if (!confirmation.isConfirmed) {
-      Swal.fire({
-        icon: "info",
-        title: "Opération annulée",
-        text: "La modification du projet a été annulée.",
-        showConfirmButton: false,
-        timer: 2000,
-      });
-      return;
-    }
-
     try {
       const formDataToSend = new FormData();
       for (const key in formData) {
         if (Array.isArray(formData[key])) {
           formData[key].forEach((file) => formDataToSend.append(key, file));
         } else {
-          const value =
-            key === "description" ? marked(formData[key]) : formData[key];
-          formDataToSend.append(key, value);
+          formDataToSend.append(key, formData[key]);
         }
       }
 
-      formDataToSend.append("currentImages", JSON.stringify(currentImages));
-      formDataToSend.append("currentCover", currentCover);
+      formDataToSend.append(
+        "deletedImages",
+        JSON.stringify(formData.deletedImages)
+      );
 
       const token = localStorage.getItem("token");
       if (!token) throw new Error("No token available.");
@@ -157,26 +134,41 @@ const EditProjectForm = () => {
         })
         .json();
 
-      console.log("Projet modifié avec succès:", response);
-
+      console.log("Projet mis à jour avec succès:", response);
       Swal.fire({
         icon: "success",
-        title: "Projet modifié !",
-        text: "Le projet a bien été modifié avec succès.",
+        title: "Projet mis à jour !",
+        text: "Le projet a bien été mis à jour avec succès.",
         showConfirmButton: false,
         timer: 2000,
       });
-      router.push(`/pages/projects/${id}`);
+      router.push("/");
     } catch (error) {
-      console.error("Erreur lors de la modification du projet:", error);
+      console.error("Erreur lors de la mise à jour du projet:", error);
       Swal.fire({
         icon: "error",
         title: "Erreur",
-        text: `Une erreur est survenue lors de la modification du projet: ${error.message}`,
+        text: `Une erreur est survenue lors de la mise à jour du projet: ${error.message}`,
         showConfirmButton: false,
         timer: 2000,
       });
     }
+  };
+
+  const resetForm = () => {
+    setFormData({
+      userId: formData.userId,
+      title: "",
+      categories: "",
+      cover: null,
+      images: [],
+      shortDescription: "",
+      description: "",
+      year: "",
+      skills: "",
+      url: "",
+      deletedImages: [],
+    });
   };
 
   const renderInputField = (
@@ -222,9 +214,9 @@ const EditProjectForm = () => {
         className="w-4/5 sm:w-3/5 md:w-3/5 lg:w-3/5 m-auto flex flex-col gap-4 bg-background/60 p-6 rounded-lg shadow-pxl"
       >
         <div className="w-full flex flex-col gap-2">
-          <h1 className="text-3xl text-center">Modifier le Projet</h1>
+          <h1 className="text-3xl text-center">Modifier un Projet</h1>
           <p className="text-xs italic text-center">
-            Modifiez les informations du projet ci-dessous.
+            Modifiez les champs pour mettre à jour le projet.
           </p>
         </div>
         <Spacing size={20} />
@@ -254,72 +246,75 @@ const EditProjectForm = () => {
             </select>
           </div>
         </div>
-        <div className="flex flex-col gap-2 p-4 bg-secondary/50 rounded-lg border boder-foreground/10">
-          <Label>Image de couverture actuelle</Label>
-          {currentCover ? (
-            <div className="relative">
-              <img
-                src={currentCover}
-                alt="Image de couverture"
-                className="rounded-lg"
-              />
-              <Button
-                type="button"
-                className="absolute top-2 right-2 p-2 hover:bg-secondary/60 transition-colors duration-500 ease-in-out"
-                onClick={handleDeleteCover}
-              >
-                <MdDeleteForever size={20} />
-              </Button>
-            </div>
-          ) : (
-            <p className="py-4 text-red-500">Aucune image de couverture</p>
-          )}
-        </div>
-        <div className="flex flex-col gap-2">
-          <Label htmlFor="cover" className="truncate">
-            Nouvelle image de couverture
-          </Label>
-          <Input
-            className="lg:pb-14 lg:pt-10 cursor-pointer text-center bg-secondary/50 text-current"
-            type="file"
-            name="cover"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex flex-col gap-2 p-4 bg-secondary/50 rounded-lg border border-foreground/10">
-          <Label>Images actuelles</Label>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {currentImages.length > 0 ? (
-              currentImages.map((image, index) => (
-                <div key={index} className="relative">
+        <div className="grid grid-cols-1 gap-5 text-center lg:grid lg:grid-cols-2 lg:gap-10">
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="cover" className="truncate">
+              Image de couverture
+            </Label>
+            <Input
+              className="lg:pb-14 lg:pt-10 cursor-pointer text-center bg-secondary/50 text-current"
+              type="file"
+              name="cover"
+              onChange={handleChange}
+              // required
+            />
+            <div className="flex rounded-sm overflow-hidden gap-2 mt-2">
+              {formData.cover && (
+                <div className="relative rounded-sm overflow-hidden">
                   <img
-                    src={image}
-                    alt={`Project image ${index}`}
-                    className="rounded-lg"
+                    src={
+                      formData.cover instanceof File
+                        ? URL.createObjectURL(formData.cover)
+                        : formData.cover
+                    }
+                    alt="Cover"
+                    className="object-cover"
                   />
-                  <Button
+                  <button
                     type="button"
-                    className="absolute top-2 right-2 p-2 hover:bg-secondary/60 transition-colors duration-500 ease-in-out"
-                    onClick={() => handleDeleteImage(image)}
+                    className="absolute top-2 right-2 px-2 bg-red-500 text-white text-sm rounded"
+                    onClick={() => handleRemoveCover()}
                   >
-                    <MdDeleteForever size={20} />
-                  </Button>
+                    X
+                  </button>
                 </div>
-              ))
-            ) : (
-              <p className="py-4 text-red-500">Aucune image</p>
-            )}
+              )}
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col gap-2">
-          <Label htmlFor="images">Ajouter des images</Label>
-          <Input
-            className="lg:pb-14 lg:pt-10 cursor-pointer text-center bg-secondary/50 text-current"
-            type="file"
-            name="images"
-            onChange={handleChange}
-            multiple
-          />
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="images">Images</Label>
+            <Input
+              className="lg:pb-14 lg:pt-10 cursor-pointer text-center bg-secondary/50 text-current"
+              type="file"
+              name="images"
+              onChange={handleChange}
+              multiple
+              // required
+            />
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-2 gap-2 mt-2">
+              {formData.images.map((image, index) => (
+                <div
+                  key={index}
+                  className="relative rounded-sm overflow-hidden"
+                >
+                  <img
+                    src={
+                      image instanceof File ? URL.createObjectURL(image) : image
+                    }
+                    alt={`Image ${index + 1}`}
+                    className="object-cover"
+                  />
+                  <button
+                    type="button"
+                    className="absolute top-2 right-2 px-2 bg-red-500 text-white text-sm rounded"
+                    onClick={() => handleRemoveImage(image)}
+                  >
+                    X
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
         {renderTextareaField(
           "shortDescription",
@@ -360,7 +355,7 @@ const EditProjectForm = () => {
         <Divider className="w-4/5 lg:w-3/5 h-[0.5px] m-auto bg-primary rounded" />
         <Spacing size={5} />
         <Button type="submit" className="w-[100px] lg:w-2/5 m-auto">
-          Modifier
+          Soumettre
         </Button>
       </form>
       <Spacing size={100} />
